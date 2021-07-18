8c77a5f2563509b7d1ac4a418f727fe3380628a5
Kohei Takahashi <flast@flast.jp>
2021-07-18 10:54:18 +0900

    Added tests for flat map with tied sequence

---
+++ test/multimap_tie.cpp
@@ -0,0 +1,16 @@
+// Copyright (c) 2021 Kohei Takahashi
+// This software is released under the MIT License, see LICENSE.
+
+#include "flat_map/flat_multimap.hpp"
+#include "flat_map/tied_sequence.hpp"
+
+#include <vector>
+
+template <typename T>
+using CONTAINER = flat_map::tied_sequence<std::vector<std::tuple_element_t<0, T>>, std::vector<std::tuple_element_t<1, T>>>;
+
+#define FLAT_MAP 1
+#define MULTI_CONTAINER 1
+#include "test_case/basic.ipp"
+#include "test_case/stateful_comparison.ipp"
+#include "test_case/std.ipp"
