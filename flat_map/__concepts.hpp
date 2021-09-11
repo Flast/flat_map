9af462a864922c46991b5d47d9b33a3f38e08e14
Kohei Takahashi <flast@flast.jp>
2021-09-11 12:22:03 +0900

    Added concept like type predicator

---
+++ flat_map/__concepts.hpp
@@ -0,0 +1,32 @@
+// Copyright (c) 2021 Kohei Takahashi
+// This software is released under the MIT License, see LICENSE.
+
+#pragma once
+
+#include <utility>
+
+namespace flat_map::concepts
+{
+
+namespace detail
+{
+
+template <typename...> using bool_t = bool;
+
+#define FLAT_MAP_DEFINE_CONCEPT(Name, T, Params, ...) \
+template <typename> struct Name ## _body; \
+template <typename R, typename... Args> struct Name ## _body<R(Args...)> \
+{ \
+      template <typename T> static constexpr auto check_impl Params -> decltype(__VA_ARGS__); \
+      template <typename D = R> static constexpr detail::bool_t<decltype(check_impl<D>(std::declval<Args>()...))> check(int) { return true; } \
+      template <typename = R> static constexpr bool check(...) { return false; } \
+}; \
+template <typename T> inline constexpr bool Name = Name ## _body<T Params>::check(0)
+
+} // namespace flat_map::detail
+
+FLAT_MAP_DEFINE_CONCEPT(Reservable, T, (T c, size_t n), c.reserve(n));
+FLAT_MAP_DEFINE_CONCEPT(HasCapacity, T, (T c), c.capacity());
+FLAT_MAP_DEFINE_CONCEPT(Shrinkable, T, (T c), c.shrink_to_fit());
+
+} // namespace flat_map::concepts
