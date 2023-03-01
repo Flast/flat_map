8d13c8ae1e549d606c57107ecab752cc49bebb86
Kohei Takahashi <flast@flast.jp>
2023-03-01 21:00:17 +0900

    Update Catch2 to v3

--- cmake/catch2.cmake
+++ cmake/catch2.cmake
@@ -1,27 +1,20 @@
-find_package(Catch2 QUIET)
+find_package(Catch2 3 QUIET)
 
-if(Catch2_FOUND)
-  add_library(Catch2Target ALIAS Catch2::Catch2)
-else()
-  include(ExternalProject)
+if(NOT Catch2_FOUND)
+  include(FetchContent)
 
-  ExternalProject_Add(
-    Catch2Build
+  FetchContent_Declare(
+    Catch2
     GIT_REPOSITORY https://github.com/catchorg/catch2
-    GIT_TAG v2.13.6
-    PREFIX external
-    CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${CMAKE_BINARY_DIR}/external
+    GIT_TAG v3.3.2
   )
 
-  add_library(Catch2Target INTERFACE)
-  target_include_directories(Catch2Target INTERFACE ${CMAKE_BINARY_DIR}/external/include)
-  add_dependencies(Catch2Target Catch2Build)
+  FetchContent_MakeAvailable(Catch2)
 endif()
 
 function(add_catch2_test testname)
   add_executable(${testname} ${ARGN})
-  target_link_libraries(${testname} Catch2Target)
-  target_compile_options(${testname} PRIVATE "-DCATCH_CONFIG_MAIN")
+  target_link_libraries(${testname} PRIVATE Catch2::Catch2WithMain)
   add_test(NAME ${testname} COMMAND ${testname})
   set_tests_properties(${testname} PROPERTIES LABELS ${testname} TIMEOUT 30)
   set_target_properties(${testname} PROPERTIES EXCLUDE_FROM_ALL 1 EXCLUDE_FROM_DEFAULT_BUILD 1)
