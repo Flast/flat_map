8010355ba015ecc4711bc9b66d0e84a944787d1b
Kohei Takahashi <flast@flast.jp>
2024-05-30 11:56:12 +0900

    Bump catch2 version

--- cmake/catch2.cmake
+++ cmake/catch2.cmake
@@ -6,7 +6,7 @@ if(NOT Catch2_FOUND)
   FetchContent_Declare(
     Catch2
     GIT_REPOSITORY https://github.com/catchorg/catch2
-    GIT_TAG v3.3.2
+    GIT_TAG v3.5.4
   )
 
   FetchContent_MakeAvailable(Catch2)
